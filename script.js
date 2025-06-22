98de873216d737abe0bfb53e8c5791e2d40ebd10
carguacundodaniel <dcarguacundo@est.ups.edu.ec>
2025-06-21 20:43:21 -0500

    feat: agregar movimiento automático de la pelota con rebotes

--- script.js
+++ script.js
@@ -19,3 +19,40 @@ function draw() {
 }
 
 setInterval(draw, 1000 / 60);
+<<<<<<< HEAD
+=======
+
+let ballX = 400;
+let ballY = 250;
+let ballSpeedX = 4;
+let ballSpeedY = 3;
+
+function draw() {
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  // Paddle fijo
+  ctx.fillStyle = "white";
+  ctx.fillRect(10, 200, 10, 100);
+
+  // Pelota en movimiento
+  ctx.beginPath();
+  ctx.arc(ballX, ballY, 10, 0, Math.PI * 2);
+  ctx.fillStyle = "white";
+  ctx.fill();
+  ctx.closePath();
+
+  // Movimiento
+  ballX += ballSpeedX;
+  ballY += ballSpeedY;
+
+  // Rebotes
+  if (ballY + 10 > canvas.height || ballY - 10 < 0) {
+    ballSpeedY *= -1;
+  }
+
+  if (ballX + 10 > canvas.width || ballX - 10 < 0) {
+    ballSpeedX *= -1;
+  }
+}
+>>>>>>> ebb7f90 (feat: agregar movimiento automático de la pelota con rebotes)
