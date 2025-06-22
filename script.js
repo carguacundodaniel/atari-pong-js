59e8318da483211c1533ce2e39b00270e0598bb4
carguacundodaniel <dcarguacundo@est.ups.edu.ec>
2025-06-21 20:33:33 -0500

    feat: dibujar paddle y pelota estáticos en canvas

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-<<<<<<< HEAD
-console.log("Juego Pong iniciado");
-=======
-console.log("Juego cargado");
->>>>>>> b76b622 (feat: estructura base del juego Pong con HTML, CSS y JS)
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+function draw() {
+  // Fondo negro
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  // Paddle
+  ctx.fillStyle = "white";
+  ctx.fillRect(10, 200, 10, 100);
+
+  // Pelota
+  ctx.beginPath();
+  ctx.arc(400, 250, 10, 0, Math.PI * 2);
+  ctx.fillStyle = "white";
+  ctx.fill();
+  ctx.closePath();
+}
+
+setInterval(draw, 1000 / 60);
